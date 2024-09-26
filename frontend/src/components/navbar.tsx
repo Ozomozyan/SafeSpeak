2935b2f06a1bdacc0fad6dbbae35c5aeedfdcf9c
chris <christophe.zhang@ecoles-epsi.net>
2024-09-26 17:38:54 +0200

    fix

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -89,7 +89,7 @@ const Navbar: React.FC = () => {
                     <ul
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                        <Link href="/accueil"><li><a>Accueil</a></li></Link>
+                        <li><Link href="/accueil">Accueil</Link></li>
                         <li><a onClick={handleLogout}>Logout</a></li>
                     </ul>
                 </div>
